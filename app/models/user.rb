71581787bf082ad7c52100af10451c43004d056d
iAnishka <prettygirler@gmail.com>
2013-10-04 16:12:06 +0530

    Removed forgot password function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # 
+  #### Here I commented recoverable to remove "forgot password" feature.
+  devise :database_authenticatable, :registerable, #:recoverable, 
+         :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name
